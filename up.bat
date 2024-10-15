9881ae41cc1b2fe00e1c18a9af74ca30d38dc09d
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-10-15 16:56:20 +0200

    updated

--- up.bat
+++ up.bat
@@ -1,10 +1,14 @@
 :: up.bat windows | up.sh linux,Mac
 
+:: Subdomain muss voher eingerichtet sein
+
 :: win
 git pull
 git add .
 git commit -m "%*"
 git push
+::ng build - bei Angular | # Das ist eine Alternative, wenn "git ftp push" nicht ausgeführt wird : call ng build
+git ftp push
 
 
 :: lin
